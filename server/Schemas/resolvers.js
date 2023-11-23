5cbc1d485add5590cc8450ec0c589421d6eb693d
Robert Kaiser <paintballmanrob@gmail.com>
2023-11-23 08:43:54 -0500

    Add resolvers for user queries and mutations

---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,63 @@
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, { username, id }, context) => {
+      const foundUser = await User.findOne({
+        $or: [{ _id: id }, { username: username }],
+      });
+
+      if (!foundUser) {
+        throw new Error('Cannot find a user with this id!');
+      }
+
+      return foundUser;
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ $or: [{ username }, { email: username }] });
+      if (!user) {
+        throw new Error("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new Error('Wrong password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookData } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new Error('You need to be logged in!');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new Error('You need to be logged in!');
+    },
+  },
+};
+module.exports = resolvers;

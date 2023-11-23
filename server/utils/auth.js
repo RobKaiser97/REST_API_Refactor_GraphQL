a374f55398317601fd3a87521489b65effdaa28b
Robert Kaiser <paintballmanrob@gmail.com>
2023-11-23 08:44:01 -0500

    Update token verification in authMiddleware function

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -2,13 +2,12 @@ const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
-const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -21,11 +20,11 @@ module.exports = {
 
     // verify token and get user data out of it
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      const { data } = jwt.verify(token, secret, { maxAge: '2h' });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      return res.status(401).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint

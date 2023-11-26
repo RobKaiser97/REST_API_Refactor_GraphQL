dfc24fd10c876c58afaf55c2bd0df22b6c1592d7
Robert Kaiser <paintballmanrob@gmail.com>
2023-11-25 21:24:56 -0500

    Refactor server.js file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const { ApolloServerPluginUsageReporting } = require('apollo-server-core');
 const path = require('path');
 const { typeDefs, resolvers } = require('./Schemas');
 const db = require('./config/connection');
@@ -12,29 +11,26 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
-  plugins: [
-    ApolloServerPluginUsageReporting({
-      sendVariableValues: { all: true },
-    }),
-  ],
 });
 
-server.applyMiddleware({ app });
+server.start().then(() => {
+  server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
   });
 });

3a140661ee06bae3404e9ccee545f31654d60164
Robert Kaiser <paintballmanrob@gmail.com>
2023-11-27 20:22:27 -0500

    Add Apollo Client and authentication to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,46 @@
+// Importing necessary styles and components
 import "./App.css";
 import { Outlet } from "react-router-dom";
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import Navbar from "./components/Navbar";
-import { ApolloProvider } from "@apollo/client";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+// Creating an HTTP link for the Apollo Client
+const httpLink = createHttpLink({ uri: "/graphql" });
+
+// Creating an authentication link to attach the token to the request headers
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      // Attaching the token to the authorization header
+      authorization: token ? `Bearer: ${token}` : "",
+    },
+  };
+});
+
+// Creating an instance of ApolloClient with the authentication link and cache
 const client = new ApolloClient({
-  uri: "/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
+// Main App component
 function App() {
   return (
-    <>
-      <ApolloProvider client={client}>
-        <Navbar />
-        <Outlet />
-      </ApolloProvider>
-    </>
+    // Providing the ApolloClient instance to the React app
+    <ApolloProvider client={client}>
+      <Navbar />
+      <Outlet />
+    </ApolloProvider>
   );
 }
 
+// Exporting the App component
 export default App;

af198ba9d1a1f5b89bf58468feb2efab97886808
Robert Kaiser <paintballmanrob@gmail.com>
2023-11-27 20:22:37 -0500

    Refactor type definitions for User, Book, Auth, and BookInput

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -1,14 +1,16 @@
 const typeDefs = `
+# Define which fields are accessible from the 'User' type
   type User {
-    _id: ID!
-    username: String!
-    email: String!
+    _id: ID
+    username: String
+    email: String
     bookCount: Int
     savedBooks: [Book]
   }
 
+# Define which fields are accessible from the 'Book' type
   type Book {
-    bookId: String!
+    bookId: String
     authors: [String]
     description: String
     title: String
@@ -16,24 +18,29 @@ const typeDefs = `
     link: String
   }
 
+# Define which fields are accessible from the 'Auth' type
   type Auth {
     token: ID!
     user: User
   }
 
+# Define which fields are accessible from the 'BookInput' type
   input BookInput {
+    bookId: String
     authors: [String]
     description: String
     title: String
-    bookId: String
     image: String
     link: String
   }
 
+# Define which queries the front end can make to the back end
   type Query {
+    # Me query returns a User object if there's a user in the context
     me: User
   }
 
+# Define which mutations the front end can make to the back end
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
@@ -42,4 +49,5 @@ const typeDefs = `
   }
 `;
 
+// Export the typeDefs
 module.exports = typeDefs;

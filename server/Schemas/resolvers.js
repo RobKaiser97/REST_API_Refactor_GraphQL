b325f92ce02347c0435f675a29e33cacda603006
Robert Kaiser <paintballmanrob@gmail.com>
2023-11-27 20:22:34 -0500

    Add user authentication and book management functionality

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,85 +1,56 @@
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
 const resolvers = {
   Query: {
-    me: async (parent, { username, id }, context) => {
-      try {
-        const foundUser = await User.findOne({
-          $or: [{ _id: id }, { username: username }],
-        });
-
-        if (!foundUser) {
-          throw new Error('Cannot find a user with this id!');
-        }
-
-        return foundUser;
-      } catch (error) {
-        console.error(error);
-        throw new Error('An error occurred while fetching the user.');
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
       }
+      throw new Error('You need to be logged in!');
     },
   },
   Mutation: {
-    addUser: async (parent, args) => {
-      try {
-        const user = await User.create(args);
-        const token = signToken(user);
-        return { token, user };
-      } catch (error) {
-        console.error(error);
-        throw new Error('An error occurred while creating the user.');
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new Error('No user found with this email address');
       }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new Error('Incorrect password');
+      }
+      const token = signToken(user);
+      return { token, user };
     },
-    login: async (parent, { username, password }) => {
-      try {
-        const user = await User.findOne({ $or: [{ username }, { email: username }] });
-        if (!user) {
-          throw new Error("Can't find this user");
-        }
-
-        const correctPw = await user.isCorrectPassword(password);
-
-        if (!correctPw) {
-          throw new Error('Wrong password!');
-        }
-
-        const token = signToken(user);
-        return { token, user };
-      } catch (error) {
-        console.error(error);
-        throw new Error('An error occurred while logging in.');
-      }
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
     saveBook: async (parent, { bookData }, context) => {
-      try {
-        if (context.user) {
-          const updatedUser = await User.findOneAndUpdate(
-            { _id: context.user._id },
-            { $addToSet: { savedBooks: bookData } },
-            { new: true, runValidators: true }
-          );
-          return updatedUser;
-        }
-        throw new Error('You need to be logged in!');
-      } catch (error) {
-        console.error(error);
-        throw new Error('An error occurred while saving the book.');
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookData } },
+          { new: true }
+        );
+        return updatedUser;
       }
+      throw new Error('You need to be logged in!');
     },
     removeBook: async (parent, { bookId }, context) => {
-      try {
-        if (context.user) {
-          const updatedUser = await User.findOneAndUpdate(
-            { _id: context.user._id },
-            { $pull: { savedBooks: { bookId } } },
-            { new: true }
-          );
-          return updatedUser;
-        }
-        throw new Error('You need to be logged in!');
-      } catch (error) {
-        console.error(error);
-        throw new Error('An error occurred while removing the book.');
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
       }
+      throw new Error('You need to be logged in!');
     },
   },
 };
+
 module.exports = resolvers;

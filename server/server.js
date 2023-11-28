a29e261d3cedaae993d78c001163477a0bc1a590
Robert Kaiser <paintballmanrob@gmail.com>
2023-11-27 20:32:42 -0500

    Fix import case in server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { expressMiddleware } = require("@apollo/server/express4"); // Import Apol
 const path = require("path");
 const { authMiddleware } = require("./utils/auth"); // Import authentication middleware
 
-const { typeDefs, resolvers } = require("./schemas"); // Import GraphQL type definitions and resolvers
+const { typeDefs, resolvers } = require("./Schemas"); // Import GraphQL type definitions and resolvers
 const db = require("./config/connection"); // Import database connection settings
 
 const PORT = process.env.PORT || 3001; // Define the port number for the server
@@ -22,7 +22,7 @@ const startApolloServer = async () => {
   app.use(express.json());
 
   // Use Apollo Server Express middleware for handling GraphQL requests
-  app.use("/graphql", expressMiddleware(server, { context: authMiddleware }));
+  app.use('/graphql', expressMiddleware(server, { context: authMiddleware }));
 
   if (process.env.NODE_ENV === "production") {
     // Serve static files in production mode (e.g., React frontend)
